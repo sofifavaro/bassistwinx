0b83730210fa26bc9337267cca7792e9ae00c555
eleonoramontello <eleonora.montello@student.h-farm.com>
2021-12-15 11:34:35 +0100

    checked with pep8

--- bio.py
+++ bio.py
@@ -1,11 +1,11 @@
-
-   
 """
 The function of the bio module
 is to explain, in the most understandable way possible,
 who was the bassist with his/her relative
-band in which he/she played, when he/she lived, if he/she had a stage name,
-the genre they played, his/her nationality, if he/she played as a solist or not,
+band in which he/she played, when he/she lived,
+if he/she had a stage name,
+the genre they played, his/her nationality,
+if he/she played as a solist or not,
 and the period in which he/she was most famous.
 The function first checks if the input
 the user inserted is present in
@@ -16,6 +16,7 @@ the Wikipedia's link.
 """
 
 import pandas as pd
+
 
 def return_bio(bassist):
     """
@@ -30,22 +31,26 @@ def return_bio(bassist):
 
     if bassist in bassists:
         print(db["Name"].loc[
-            db["Name"].str.lower() == bassist.lower()].values[0], "is a/an", db[
+            db["Name"].str.lower() == bassist.lower()].values[
+            0], "is a/an", db[
                 "Nationality"].loc[
                     db["Name"].str.lower() == bassist.lower()].values[
                     0], "bassist who lived in these years:", db["Life"].loc[
                     db["Name"].str.lower() == bassist.lower()].values[
                     0], "The artist played in", db["Band"].loc[
                         db["Name"].str.lower() == bassist.lower()].values[
-                            0], " and he/she was most famous in the" , db[
+                            0], " and he/she was most famous in the", db[
                                 "Period"].loc[db["Name"].str.lower(
                                     ) == bassist.lower()].values[
-                                        0], "s. He/She belongs to the genre of",  db[
+                                        0], "s. He/She belongs to
+                                        the genre of", db[
                                             "Genre"].loc[
                                                 db["Name"].str.lower() ==
             bassist.lower()].values[0], "He/She", db[
-                "Solist"].loc[db["Name"].str.lower() == bassist.lower()].values[
-                    0], "played as a soloist, and He/She", db["Stage Name"].loc[
+                "Solist"].loc[db["Name"].str.lower() ==
+                 bassist.lower()].values[
+                    0], "played as a soloist, and He/She", db[
+                    "Stage Name"].loc[
                         db["Name"].str.lower() == bassist.lower(
                             )].values[0], "a stage name. Here you can find" +
                                           " the web link" +
@@ -56,10 +61,10 @@ def return_bio(bassist):
                                               ) == bassist.lower()].values[0])
     else:
         print(bassist + " seems not to be present in our database." +
-                       "Are you sure that you wrote" +
-                       " the name of the artist correctly?" +
-                       " Use -d to check if it is already in our database," +
-                       "maybe there is a spelling error in the input." +
-                       " Check it and then try again, if it is not present," +
-                       "use -a to insert the new artist," +
-                       " thank you for your patience!")
+                        "Are you sure that you wrote" +
+                        " the name of the artist correctly?" +
+                        " Use -d to check if it is already in our database," +
+                        "maybe there is a spelling error in the input." +
+                        " Check it and then try again, if it is not present," +
+                        "use -a to insert the new artist," +
+                        " thank you for your patience!")
